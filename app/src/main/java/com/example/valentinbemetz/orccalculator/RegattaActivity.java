eb9e4792000358439fcd4ffd282615d80d1ab62e
valentinbemetz <valentin.bemetz@gmail.com>
2018-06-04 21:59:12 +0200

    added static column and row titles

--- app/src/main/java/com/example/valentinbemetz/orccalculator/RegattaActivity.java
+++ app/src/main/java/com/example/valentinbemetz/orccalculator/RegattaActivity.java
@@ -2,6 +2,9 @@ package com.example.valentinbemetz.orccalculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -9,6 +12,10 @@ import android.widget.TextView;
 public class RegattaActivity extends AppCompatActivity {
 
     TableLayout table;
+    ScrollView scrollView, tableScrollVert;
+    HorizontalScrollView horizontalScrollView, tableScrollHor;
+    TableLayout tableRow;
+    TableLayout tableColumn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,12 +23,23 @@ public class RegattaActivity extends AppCompatActivity {
         setContentView(R.layout.activity_regatta);
 
         table = findViewById(R.id.table_regatta);
+        scrollView = findViewById(R.id.sv_row);
+        tableScrollHor = findViewById(R.id.table_scrollHor);
+        tableScrollVert = findViewById(R.id.table_scrollVert);
+        horizontalScrollView = findViewById(R.id.sv_column);
+        tableRow = findViewById(R.id.table_row);
+        tableColumn = findViewById(R.id.table_column);
 
-        for(int r = 1; r <= 100; r++){
+        tableRow.removeAllViews();
+        tableColumn.removeAllViews();
+
+
+        for(int r = 1; r <= 20; r++){
             TableRow row = new TableRow(this.getApplicationContext());
-            for (int c = 1; c <= 10; c++){
+            for (int c = 1; c <= 5; c++){
                 TextView tv = new TextView(this.getApplicationContext());
-                tv.setText("Das ist ein Text "+String.valueOf(10*(r-1)+c));
+                //tv.setText("Das ist ein Text "+String.valueOf(10*(r-1)+c));
+                tv.setText("TestTestTest");
                 tv.setPadding(16,16,16,16);
                 if (r%2==0){
                     if(c%2==0) {
@@ -40,5 +58,31 @@ public class RegattaActivity extends AppCompatActivity {
             }
             table.addView(row);
         }
+        table.
+
+        for (int i = 0; i<table.getChildCount(); i++){
+            TableRow row = (TableRow) table.getChildAt(i);
+            if (i==0){
+                TableRow row1 = new TableRow(this);
+                for (int j = 0; j < row.getChildCount(); j++) {
+                    View tv = row.getChildAt(j);
+                    TextView tv1 = new TextView(this);
+                    tv1.setText("Column "+String.valueOf(j));
+                    tv1.setPadding(16,16,16,16);
+                    row1.addView(tv1);
+                }
+                row1.setLayoutParams(row.getLayoutParams());
+                tableColumn.addView(row1);
+            }
+            TextView tv = (TextView)((TableRow)table.getChildAt(i)).getChildAt(0);
+            TextView tv1 = new TextView(this);
+            tv1.setText("Row "+String.valueOf(i));
+            tv1.setPadding(16,16,16,16);
+            TableRow row1 = new TableRow(this);
+            row1.addView(tv1);
+            tableRow.addView(row1);
+        }
+
+
     }
 }
